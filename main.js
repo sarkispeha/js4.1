910dbbd6ba9f69997df86de273981af53624fd6a
Sarkis Peha <sarkispeha@gmail.com>
2014-09-17 12:20:46 -0600

    finish part1

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).on('ready', function() {
-  
+
+	$('.button1').click(function() {
+		$('body').append('You clicked a button');
+	});
+
+	$('.button2').click(function() {
+		$('body').append('<h1>You clicked a button</h1>');
+	});
+
+	$('.button3').click(function() {
+		$('body').append('<ul><li>item1</li><li>item2</li><li>item3</li></ul>');
+	});
+
 });
+
+
