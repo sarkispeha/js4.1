b08a2a43e80b4f994e9a846a543d7e1e3627967d
Sarkis Peha <sarkispeha@gmail.com>
2014-09-17 17:10:17 -0600

    finished part3

--- main3.js
+++ main3.js
@@ -1,4 +1,31 @@
 $(document).on('ready', function() {
 
+$('button').css({
+		width: 500,
+		height: 500,
+		fontSize: 40,
+		position: 'absolute',
+		padding: '25%',
+		margin: '5%',
+});
+
+$('button').click(function() {
+	$('body').append('<div class="popup"><p>This is some HTML content</p><button>Close</button></div>')
+	$('.popup').css({
+		position: 'fixed',
+		width: 200,
+		height: 200,
+		backgroundColor: 'blue',
+		margin: '35%',
+	})
+	$('.popup button').click(function(){
+		$('.popup').hide();
+	})
+
+});
+
+
+
+
 
 	});
